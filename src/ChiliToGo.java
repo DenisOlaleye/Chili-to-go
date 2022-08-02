457368dc929e64b5898089123585b4930dc65e00
DenisOlaleye <denis.olaleye@gmail.com>
2022-08-02 17:08:19 +0300

    2.0

--- src/ChiliToGo.java
+++ src/ChiliToGo.java
@@ -3,6 +3,12 @@ dinners to go. The price is $7 for an adult meal and $4 for a child’s meal. Wr
 program that accepts the number of each type of meal ordered and display the
 total money collected for adult meals, children’s meals, and all meals. Save the
 program as ChiliToGo.java.*/
+//----------------
+//----------------
+/*In the ChiliToGo program in Exercise 12, the costs to produce an adult meal and
+a child’s meal are $4.35 and $3.10, respectively. Modify the ChiliToGo program to
+display the total profit for each type of meal as well as the grand total profit. Save
+the program as ChiliToGoProfit.java.*/
 
 import java.util.Scanner;
 
@@ -11,14 +17,14 @@ public class ChiliToGo
 {
 	public static void main (String [] args)
 	{
-		final int ADULT_PRICE = 7;
-		final int CHILD_PRICE = 4;
+		final double ADULT_PRICE = 4.35;
+		final double CHILD_PRICE = 3.1;
 		
 		int adultMeal;
 		int childMeal;
-		int adultMealMoney;
-		int childrenMealMoney;
-		int totalMoney;
+		double adultMealMoney;
+		double childrenMealMoney;
+		double totalMoney;
 		
 		Scanner input = new Scanner(System.in);
 		
